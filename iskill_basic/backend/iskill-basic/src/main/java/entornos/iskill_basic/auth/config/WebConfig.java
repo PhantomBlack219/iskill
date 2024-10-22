649a1e7386f78177ab1044c4eccdc0f9ee2f4398
Juan David Escalante <101756457+UntetheredJ@users.noreply.github.com>
2024-10-22 15:43:09 -0500

    Added CORS mapping.

---
+++ iskill_basic/backend/iskill-basic/src/main/java/entornos/iskill_basic/auth/config/WebConfig.java
@@ -0,0 +1,17 @@
+package entornos.iskill_basic.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("null") // Allow local file origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
